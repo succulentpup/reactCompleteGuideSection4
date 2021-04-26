a4f0824a66580ac4e2cbae53c0d10d11484aaa78
Ganesh Adapa <ganesh.adapa@mysense.ai>
2021-04-26 10:57:34 +0100

    implemented expense form to accept the expenses.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,6 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
     setTitle('Updated!');
-    console.log({title}, 'title');
   }
 
   return (
